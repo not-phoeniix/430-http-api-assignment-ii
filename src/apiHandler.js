52f4005bc091a596df502b5324c7a9d36028b404
Nikki Murello <nmurello15@gmail.com>
2026-02-13 18:33:21 -0500

    Added extra conditions and response codes to add user endpoint

--- src/apiHandler.js
+++ src/apiHandler.js
@@ -1,15 +1,18 @@
 const users = {};
 
 function respond(req, res, status, content) {
-    const str = JSON.stringify(content);
+    let str = undefined;
+    if (content) {
+        str = JSON.stringify(content);
+    }
 
     res.writeHead(status, {
         "Content-Type": "application/json",
-        "Content-Length": Buffer.byteLength(str, "utf8")
+        "Content-Length": str ? Buffer.byteLength(str, "utf8") : 0
     });
 
-    // HEAD requests and update codes (204) don't get a response body
-    if (req.method !== "HEAD" && status !== 204) {
+    // HEAD requests, empty content, and update codes (204) don't get a response body
+    if (str !== undefined && req.method !== "HEAD" && status !== 204) {
         res.write(str);
     }
 
@@ -33,11 +36,30 @@ function getUsers(req, res) {
 
 function addUser(req, res, body) {
     const { name, age } = body;
-    users[name] = { name, age };
 
-    respond(req, res, 201, {
-        message: "Created Successfully"
-    });
+    // default to bad request code
+    let response = {
+        message: "Name and age are both required!",
+        id: "addUserMissingParams"
+    };
+    let status = 400;
+
+    // update an existing user
+    if (name && users[name] && age !== undefined) {
+        users[name].age = age;
+        status = 204;
+        response = undefined;
+    }
+
+    // create a new user
+    if (name && !users[name] && age !== undefined) {
+        users[name] = { name, age };
+        status = 201;
+        response.message = "Created successfully!";
+        response.id = undefined;
+    }
+
+    respond(req, res, status, response);
 }
 
 module.exports = {
